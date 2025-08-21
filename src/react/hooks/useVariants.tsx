e73ce447ea038a5b95f668892d02408de4520f34
John Goh <john.goh@alphawave-innovations.com>
2025-08-21 09:58:55 +0800

    feat: react, useVariants hook

---
+++ src/react/hooks/useVariants.tsx
@@ -0,0 +1,22 @@
+import { CSSProperties } from "react";
+import { combine } from "../../object";
+
+export const useVariants = <T extends ComponentVariants>(
+  selectedVariants: (keyof T)[],
+  inputVariants: T
+) => {
+  const appliedVariants = selectedVariants.map(
+    (variant) => inputVariants[variant]
+  );
+  const variantClasses = appliedVariants.map((v) => v.className);
+  const variantStyles = combine(...appliedVariants.map((v) => v.style));
+
+  return { appliedVariants, variantClasses, variantStyles };
+};
+
+export type ComponentVariants = {
+  [key: string]: {
+    className: string;
+    style: CSSProperties;
+  };
+};
